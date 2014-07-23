6b86f2e165df00fa78ecb49aaee570791d1fdbfa
Karsten Silkenbäumer <karsten.silkenbaeumer@xing.com>
2014-07-23 21:13:43 +0200

    Complete restructuring into different parts.

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ If you have https://hub.github.com/ installed and your repo is called `dotfiles`
 
 will do.
 
-If you do not want to clone your dotfiles repo into `~/Systems`, you can choose a different destination by giving a second optional argument like
+If you do not want to clone your dotfiles repo into `~/System`, you can choose a different destination by giving a second optional argument like
 
     curl -fsSL https://raw.github.com/kassi/Dotfiles.install/master/install.sh | bash -s dotfiles lib
 
@@ -26,21 +26,41 @@ This script is build to be Mac OS X specific, but will work with other linuxes a
 
 The script will clone your repo, create necessary directories and link (nearly) all files found.
 
-If files already exists as regular files, they will be moved to a backed up version and the link will be created.
+I picked up the idea of dividing the dotfiles in semantic parts from [holman/dotfiles](https://github.com/holman/dotfiles) and kept the `*.symlink` logic.
+However, I added a little more sugar.
 
-If files already exists as a link nothing will change.
-It will print out a message if the existing symbolic link doesn't point to the same file.
+* on first level you'll find directories for different parts/tools/apps. Inside
+    * any named `something.symlink` will be symlinked to
+        * `$HOME/.something` if it's on top-level
+        * the directory given if it's not
+    * any directory named `something.dir` will be created as `$HOME/something`.
+    * any file in `bin` will be symlinked to `$HOME/bin`
+    * any file in `etc` will be symlinked to `$HOME/etc`
+    * any file named in `something.bash` will be concatenated to `$HOME/.bash_something`
+    * any install.sh will be executed inside the last step of the install script
 
 ## Why
 
 Why do I need this?
 
-Because I have two dotfiles repositories plus an additional system additions repository and all installations behave the same.
-Some are public, another one is private and not on a public hoster.
-
-You may think of having your email address inside .gitconfig (public) or inside a .gitconfig.private (private repo), included by .gitconfig.
+Because I have several dotfiles repositories plus an additional system additions repository and all installations behave the same.
+Some are public, another one is private and not on a public hoster (you may think of having your email address inside `.gitconfig` (public) or inside a `.gitconfig.private` (private repo), included by `.gitconfig`).
 
 With this script I'm able to install those repos with ease.
+
+## Intention
+
+What do I want to achieve?
+
+* A lot of files have to be linked to from `$HOME` as `.filename`
+* Some directories may be linked to from `$HOME`
+* Some files have to be linked to from `$HOME/bin`
+* Some files event have to be linked to from somewhere else like `$HOME/Library/whatever` and those are not necessarily dot files.
+* There are several repositories, each one
+    * may define new `.filename`
+    * may define `.filename` for file already linked to by another installer task
+    * may define additional files for `bin` so that linking the directory instead of the files wouldn't work
+    * may have their own install scripts
 
 ## Author
 
--- install.sh
+++ install.sh
@@ -1,76 +1,313 @@
 #!/bin/bash
 #
-# Installation script for dotfiles repositories.
+# Bootstrapper for installation
+# A lot of functionality taken from https://github.com/holman/dotfiles/blob/master/script/bootstrap
 #
-# * Checks out the repo given as an optional argument into ~/Library
-#   or any other path (relative to your  home folder) given as
-#   a second optional argument.
-# * Creates all needed directories
-# * Links all files unless they are already linked.
-#   Existing regular files are backed up and replaced by the link.
+# Defaults
+path="System"
+repo="dotfiles"
+#
 
-repo="$1"
-if [[ -z "$repo" ]]; then
-  repo="dotfiles"
-fi
-name=$(basename $repo .git)
-destination="$2"
-if [[ -z "$destination" ]]; then
-  destination="Systems"
-fi
-dest_path=~/$destination/$name
+usage() {
+  printf "%b" "
+Usage
 
-if [[ -e "$dest_path" ]]; then
-  echo "Destination path '$dest_path' already exists."
-  cd "$dest_path"
-  if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
-    echo "Git repository is dirty. Skipping pull."
-  else
-    git pull --rebase
-  fi
-else
-  # checkout
-  #
-  cd ~/$destination
-  git clone $repo
-fi
+  bootstrap.sh [options] [actions]
 
-# strip /Users/Name/Library/Dotfiles[.private] from path
-subpath() {
-  echo "$1"| cut -d"/" -f6-
+or remotely
+
+  curl -fsSL https://raw.github.com/kassi/Dotfiles.bootstrap/master/bootstrap.sh | bash -s [options] [actions]
+
+Options
+
+  --path <path>
+
+    The destination path where the files will be written to.
+    Defaults to $path.
+
+  --repo <repo>
+
+    The repository to fetch instructions from.
+    Defaults to '$repo', but this is only working with a set up hub tool,
+    which is most likely not the case when setting up a machine like this.
+
+  -n | --dry-run
+
+    Do not write, just print what would be done.
+
+Actions
+
+  help    - Show this info
+  link    - Links all the (dot)files.
+  install - Runs all installations (brew, cask, indivual install.sh)
+  brew    - Bundle only Brewfile
+  cask    - Bundle only Caskfile
+  "
 }
 
-# link
-#
-cd ~
+# set -e
 
-# First create all directories
-for file in $(find $dest_path -name '.git' -prune  -o -type d -mindepth 1 -print); do
-  path=$(subpath $file)
-  if [[ -e "$path" ]]; then
-    if [[ ! -d "$path" ]]; then
-      echo "'$path' exists, but is not a directory. Aborting!"
-      exit 1
+info() { printf "  [ \033[00;34m..\033[0m ] $1\n"; }
+user() { printf "\r  [ \033[0;33m?\033[0m ] $1 "; }
+fail() { printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"; exit; }
+success() { printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"; }
+
+declare -a actions
+dry=''
+
+parse_params() {
+  while (( $# > 0 )); do
+    token="$1"
+    shift
+    case "$token" in
+      (-n|--dry-run)
+        dry="1"
+        ;;
+      (--path)
+        if [[ -n "${1:-}" ]]; then
+          path="$1"
+          shift
+        else
+          fail "--path must be followed by a path."
+        fi
+        ;;
+      (--repo)
+        if [[ -n "${1:-}" ]]; then
+          repo="$1"
+          shift
+        else
+          fail "--repo must be followed by a repository."
+        fi
+        ;;
+      (link|install|brew|cask)
+        actions+=("$token")
+        ;;
+      (help)
+        usage
+        exit 0
+        ;;
+      (*)
+        usage
+        exit 1
+        ;;
+    esac
+  done
+  if [[ -z "${actions[@]}" ]]; then
+    actions+=(link install)
+  fi
+  containsElement "install" "${actions[@]}" && actions+=(brew cask installer)
+}
+
+containsElement() {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+parse_params "$@"
+
+echo "path: $path"
+echo "repo: $repo"
+echo "actions: ${actions[@]}"
+
+NAME="$(basename "$repo" .git)"
+
+if [[ "$path" == "." ]]; then
+  DESTINATION_DIR=$(pwd)
+else
+  DESTINATION_DIR="$HOME/$path"
+fi
+DESTINATION_PATH="$DESTINATION_DIR/$NAME"
+# BASH_FILE_PATH="$DESTINATION_DIR/bash"
+
+
+link_file () {
+  local src=$1 dst=$2
+
+  echo "src: $src"
+  echo "dst: $dst"
+  # strip leading $HOME if destination is in home directory
+  dst_dir=$(dirname $dst)
+  echo "dir: $dst_dir"
+  if [[ $dst_dir == $HOME ]]; then
+    src=${src/#$HOME\//}
+    echo "new: $src"
+  fi
+  echo "now: $src"
+
+  local overwrite= backup= skip=
+  local actions=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $(basename "$dst"), what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
+        read -n 1 actions
+
+        case "$actions" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          b )
+            backup=true;;
+          B )
+            backup_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    backup=${backup:-$backup_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$backup" == "true" ]
+    then
+      mv "$dst" "${dst}.backup"
+      success "moved $dst to ${dst}.backup"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$src" "$dst"
+    success "linked $src to $dst"
+  fi
+}
+
+setup_destination () {
+  info "Setting up dotfiles system into $DESTINATION_PATH"
+  if [[ -e "$DESTINATION_PATH" ]]; then
+    info "Destination path '$DESTINATION_PATH' already exists."
+    cd "$DESTINATION_PATH"
+    if [[ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]]; then
+      info "Git repository is dirty. Skipping pull."
+    else
+      if [[ -z $dry ]]; then
+        git pull --rebase
+        success "pulled $DESTINATION_PATH"
+      fi
     fi
   else
-    mkdir -p "$path" && echo "Directory '$path' created"
-  fi
-done
-
-# Then link all files given
-for file in $(find $dest_path -name '.git' -prune -o -name 'README.md' -prune -o -type f -print); do
-  path=$(subpath $file)
-
-  if [[ -L "$path" ]]; then
-    # if file exists as a link, ignore it
-    target=$(readlink "$path")
-    if [[ "$target" != "$file" ]]; then
-      echo "Link '$path' already exists, pointing elsewhere ($target)"
+    cd "$DESTINATION_DIR"
+    if [[ -z $dry ]]; then
+      git clone $repo
+      success "cloned repository"
     fi
-    continue
-  elif [[ -f "$path" ]]; then
-    # if file exists as regular file, back it up
-    mv "$path" "$path~" && echo "Regular file '$path' backed up to '$path~'"
   fi
-  ln -s "$file" "$path" && echo "Link '$path' created"
-done
+}
+
+install_dotfiles () {
+  containsElement "link" "${actions[@]}" || return 0
+  info "Linking files"
+
+  local overwrite_all=false backup_all=false skip_all=false
+
+  OIFS="$IFS"
+  IFS=$'\n'
+  for dir in $(find "$DESTINATION_PATH" -mindepth 1 -maxdepth 1 -type d); do
+    if [[ $(basename "$dir") == ".git" ]]; then continue; fi
+    for file in $(find "$dir" -type f); do
+      base=${file/#$dir\//}
+      if [[ "$base" == "install.sh" ]]; then continue; fi
+      base_path=$(dirname "$base")
+      file_name=$(basename "$base")
+      if [[ "$base_path" == "." ]]; then base_path=""; fi
+
+      if [[ -n "$base_path" ]]; then
+        if [[ ! -d "$HOME/$base_path" ]]; then
+          if [[ -f "$HOME/$base_path" ]]; then
+            fail "directory '$HOME/$base_path' can't be created, because it exists a a file"
+          else
+            if [[ -n $dry ]]; then
+              info "mkdir -p $HOME/$base_path"
+            else
+              mkdir -p "$HOME/$base_path"
+            fi
+          fi
+        fi
+      fi
+
+      if [[ -n $dry ]]; then
+        info "Link '$base_path/$file_name' to '~/$base_path'"
+      else
+        link_file "$file" "$HOME/$base_path/$file_name"
+        success "linked '~/$base_path/$file_name' -> '$base_path/$file_name'"
+      fi
+    done
+  done
+  IFS="$OIFS"
+  info "linked files"
+}
+
+install_brewfiles () {
+  file="$1"
+  if [[ -e "$DESTINATION_PATH/$file" ]]; then
+    user "Continue with bundling $file? [y]es or [n]o?"
+    read -n 1 answer
+    echo
+    case "$answer" in
+      y)
+        info "Bundling $file"
+        brew bundle "$DESTINATION_PATH/$file"
+        success "bundled $file"
+        ;;
+    esac
+  fi
+}
+
+install_individual_files () {
+  for script in $(find "$DESTINATION_PATH" -type f -name 'install.sh'); do
+    user "Continue with installing $script? [y]es or [n]o?"
+    read -n 1 answer
+    echo
+    case "$answer" in
+      y)
+        info "Running $script"
+        pushd .
+        source $script
+        popd
+        ;;
+    esac
+  done
+}
+
+actions() {
+  setup_destination
+  install_dotfiles
+  containsElement "brew" "${actions[@]}" && install_brewfiles Brewfile
+  containsElement "cask" "${actions[@]}" && install_brewfiles Caskfile
+  install_individual_files
+}
+actions
